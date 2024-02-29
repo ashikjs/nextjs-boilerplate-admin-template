eb3bd45e079607e59c61f1bc62ad24df9d2a451e
Md Ashik <ashik@innospace.tech>
2024-02-29 10:37:57 +0600

    login form submit function update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,7 @@ export default function Login() {
     console.log(router)
 
     const submitLoginForm = () => {
+        console.log('submitLoginForm:: ')
         login(username, password)
             .then(data => {
                 console.log('Login successful:', data)
@@ -22,8 +23,8 @@ export default function Login() {
     return (
         <div className='p-4 max-w-screen-sm mx-auto h-svh flex items-center flex-col justify-center'>
             <h3 className='mb-4 pb-4 text-3xl'>Login page </h3>
-            <form>
-                <div className="pb-4">
+            <form action={submitLoginForm} className='text-center'>
+                <div className="pb-4 text-left">
                     <label htmlFor="userName" className='mb-2 font-bold block'>User Name</label>
                     <input type="text" name="price" id="userName"
                            value={username}
@@ -31,20 +32,20 @@ export default function Login() {
                            className="block w-full rounded-md border border-gray-600 py-3 pl-3 pr-10 text-gray-900 placeholder:text-gray-400"
                            placeholder="User name.."/>
                 </div>
-                <div className="pb-4">
-                    <label htmlFor="userName" className='mb-2 font-bold block'>Password</label>
-                    <input type="password" name="price" id="userName"
+                <div className="pb-4 text-left">
+                    <label htmlFor="userPassword" className='mb-2 font-bold block'>Password</label>
+                    <input type="password" name="price" id="userPassword"
                            value={password}
                            onChange={(e) => setPassword(e.target.value)}
                            className="block w-full rounded-md border border-gray-600 py-3 pl-3 pr-10 text-gray-900 placeholder:text-gray-400"
                            placeholder="User name.."/>
                 </div>
+
+                <button type='submit'
+                        className='bg-purple-700 px-12 py-3 mt-2 rounded-lg text-white'>Login
+                </button>
             </form>
 
-            <button onClick={submitLoginForm}
-                    type='submit'
-                    className='bg-purple-700 px-4 py-2 rounded-lg text-white'>Login
-            </button>
         </div>
     )
 }
